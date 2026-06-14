cd0d602bf7da74c49384b8959ba027fa1d1d9bd1
siddu-5 <sidduarnakonda@gmail.com>
2026-06-14 09:43:33 +0530

    Implemented model training and evaluation

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-df=pd.read_csv("data/student-por.csv",sep=";")
+df=pd.read_csv("data/student-por.csv")
 le=LabelEncoder()
-for column in df.columns:
-    if df[column].dtype=="object":
-        df[column]=le.fit_transform(df[column])
+for col in df.columns:
+    if df[col].dtype=="object":
+        df[col]=le.fit_transform(df[col])
 df.to_csv("data/processed_student_data.csv",index=False)
-print("Processing completed")
-print(df.head())
+print(df.columns.tolist())
+print(df.shape)
---
+++ src/train.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
+
+df=pd.read_csv("data/processed_student_data.csv")
+
+X=df.drop("G3",axis=1)
+y=df["G3"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+lr=LinearRegression()
+lr.fit(X_train,y_train)
+lr_pred=lr.predict(X_test)
+
+print("Linear Regression")
+print("MAE:",mean_absolute_error(y_test,lr_pred))
+print("R2:",r2_score(y_test,lr_pred))
+
+rf=RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
+rf.fit(X_train,y_train)
+rf_pred=rf.predict(X_test)
+
+print("\nRandom Forest")
+print("MAE:",mean_absolute_error(y_test,rf_pred))
+print("R2:",r2_score(y_test,rf_pred))
+
+pickle.dump(
+    rf,
+    open("models/student_model.pkl","wb")
+)
+
+print("\nModel Saved Successfully")
